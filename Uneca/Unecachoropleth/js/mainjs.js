4aea054be1d40f590d94493d38472a560901dfd7
Joseph Marindi <josephmarindi@gmail.com>
2016-08-29 16:18:43 +0300

    Update mainjs.js

--- Uneca/Unecachoropleth/js/mainjs.js
+++ Uneca/Unecachoropleth/js/mainjs.js
@@ -291,7 +291,7 @@ function roundRight(number){
 function renameIndicator(){
 	
 	if (expressed == "pop_all_sexes"){
-		renamed_indicator = "Total Population (Male and Female";
+		renamed_indicator = "Total Population (Male and Female) ";
 	} else if (expressed == "pop_male"){
 		renamed_indicator = "Total Male population";
 	} else if (expressed == "Malepops"){
