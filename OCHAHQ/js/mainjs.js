f458518fdb70f63d3f5b748be8b66cb973557318
Joseph Marindi <josephmarindi@gmail.com>
2016-11-15 18:36:19 +0300

    Update mainjs.js

--- OCHAHQ/js/mainjs.js
+++ OCHAHQ/js/mainjs.js
@@ -105,10 +105,10 @@
 					name: "World Average",
 					dataPoints: [
 					
-					{ x: new Date(2011), y: 2.5},
-					{ x: new Date(2012), y: 2.5},
-					{ x: new Date(2013), y: 2.5},
-					{ x: new Date(2014), y: 2.5},
+					{ x: 2011, y: 2.5},
+					{ x: 2012, y: 2.5},
+					{ x: 2013, y: 2.5},
+					{ x: 2014, y: 2.5},
 					//{ x: new Date(2015, 0), y: 2.5}
 					
 					]
@@ -120,10 +120,10 @@
 					name: "Country in Need",
 					axisYType: "secondary",
 					dataPoints: [
-					{ x: new Date(2011), y: 4.9},
-					{ x: new Date(2012), y: 5.2},
-					{ x: new Date(2013), y: 4.2},
-					{ x: new Date(2014), y: 3.6},
+					{ x: 2011, y: 4.9},
+					{ x: 2012, y: 5.2},
+					{ x: 2013, y: 4.2},
+					{ x: 2014, y: 3.6},
 					//{ x: new Date(2015, 0), y: 3.6}
 					]
 				}
