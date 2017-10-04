0ca6b8c0ea627186af745c5c7c893cf175a04c76
Joseph Marindi <josephmarindi@gmail.com>
2017-10-04 18:15:21 +0300

    Update barscode.js

--- EAA_viz/js/barscode.js
+++ EAA_viz/js/barscode.js
@@ -61,7 +61,7 @@ d3.csv("data/bar-data.csv", function(error, data) {
   svg.selectAll("bar")
       .data(data)
       .enter().append("rect")
-      .style("fill", "steelblue")
+      .style("fill", "#ff8080")
       .attr("x", function(d) { return x(d.date); })
       .attr("width", x.rangeBand())
       .attr("y", function(d) { return y(d.value); })
