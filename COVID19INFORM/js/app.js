1f24225ddf87812a9b727b79c986b087b270e344
Joseph Marindi <josephmarindi@gmail.com>
2020-04-21 14:26:24 +0300

    data

--- COVID19INFORM/js/app.js
+++ COVID19INFORM/js/app.js
@@ -106,7 +106,7 @@ var parentMetric = function(metric) {
 
 // Load data
 queue()
-    .defer(d3.csv, "data/inform-COVID-19-risk-index-v012.csv")
+    .defer(d3.csv, "data/inform-covid-19-risk-index-v012.csv")
 	//.defer(d3.csv, "INFORM_Mid2018_v034.csv")
     .await(ready);
 
