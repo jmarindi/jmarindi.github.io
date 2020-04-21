f5ee10690bf19a7e3ceaf43fc4bf8c44b44e9ee2
Joseph Marindi <josephmarindi@gmail.com>
2020-04-21 23:25:33 +0300

    New updated viz

--- COVID19INFORM/js/field-hierarchy.js
+++ COVID19INFORM/js/field-hierarchy.js
@@ -10,23 +10,51 @@ fieldHierarchy = [
   },
   {
     field: "Socio-Economic Vulnerability",
-    subset: ["Development & Deprivation", "Inequality", "Aid Dependency"]
+    subset: ["Economic Dependency Index", "Inequality", "Development & Deprivation"]
   },
   {
-    field: "Development & Deprivation",
-    subset: ["Development & Deprivation"]
+    field: "Economic Dependency Index",
+    subset: ["Public Aid per capita (US$)", "Net ODA received (% of GNI)", "Volume of remittances"]
+  },
+  {
+    field: "Public Aid per capita (US$)",
+    subset: ["Public Aid per capita (US$)"]
+  },
+  {
+    field: "Net ODA received (% of GNI)",
+    subset: ["Net ODA received (% of GNI)"]
+  },
+  {
+    field: "Volume of remittances",
+    subset: ["Volume of remittances"]
   },
   {
     field: "Inequality",
-    subset: ["Inequality"]
+    subset: ["Gender Inequality Index", "Gini Index"]
+  },
+    {
+    field: "Gender Inequality Index",
+    subset: ["Gender Inequality Index"]
+  },
+    {
+    field: "Gini Index",
+    subset: ["Gini Index"]
   },
   {
-    field: "Aid Dependency",
-    subset: ["Aid Dependency"]
+    field: "Development & Deprivation",
+    subset: ["Human Development Index", "Multidimensional Poverty Index"]
+  },
+  {
+    field: "Human Development Index",
+    subset: ["Human Development Index"]
+  },
+  {
+    field: "Multidimensional Poverty Index",
+    subset: ["Multidimensional Poverty Index"]
   },
   {
     field: "Vulnerable Groups",
-    subset: ["Uprooted people", "Health Conditions", "Children U5", "Recent Shocks", "Food Security", "Other Vulnerable Groups"]
+    subset: ["Uprooted people", "Other Vulnerable Groups", "GBV"]
   },
   {
     field: "Uprooted people",
@@ -58,59 +86,119 @@ fieldHierarchy = [
   },
   {
     field: "Institutional",
-    subset: ["DRR", "Governance"]
-  },
-  {
-    field: "DRR",
-    subset: ["DRR"]
-  },
-  {
-    field: "Governance",
     subset: ["Governance"]
   },
   {
+    field: "Governance",
+    subset: ["Corruption Perception Index", "Government Effectiveness"]
+  },
+  {
+    field: "Corruption Perception Index",
+    subset: ["Corruption Perception Index"]
+  },
+    {
+    field: "Government Effectiveness",
+    subset: ["Government Effectiveness"]
+  },
+  {
     field: "Infrastructure",
-    subset: ["Communication", "Physical infrastructure", "Access to health care"]
-  },
-  {
-    field: "Communication",
-    subset: ["Communication"]
-  },
-  {
-    field: "Physical infrastructure",
-    subset: ["Physical infrastructure"]
-  },
-  {
-    field: "Access to health care",
     subset: ["Access to health care"]
   },
   {
+    field: "Access to health care",
+    subset: ["Health Expenditure", "Immunization coverage", "Health system capacity", "Maternal Mortality Ratio"]
+  },
+    {
+    field: "Health Expenditure",
+    subset: ["per capita public and private expenditure on health care"]
+  },
+  {
+    field: "Immunization coverage",
+    subset: ["Coverage of DTP3 vaccine", "Coverage of measles-containing vaccine", "Coverage of pneumococcal conjugate vaccine"]
+  },
+   {
+    field: "Coverage of DTP3 vaccine",
+    subset: ["Coverage of DTP3 vaccine"]
+  },
+  {
+    field: "Coverage of measles-containing vaccine",
+    subset: ["Coverage of measles-containing vaccine"]
+  },
+  {
+    field: "Coverage of pneumococcal conjugate vaccine",
+    subset: ["Coverage of pneumococcal conjugate vaccine"]
+  },
+  {
+    field: "Health system capacity",
+    subset: ["Physicians Density", "Hospital bed"]
+  },
+    {
+    field: "Physicians Density",
+    subset: ["Physicians Density"]
+  },
+  {
+    field: "Hospital bed",
+    subset: ["Hospital bed"]
+  },
+  {
+    field: "Maternal Mortality Ratio",
+    subset: ["Maternal Mortality Ratio"]
+  },
+  {
     field: "COVID-19 HAZARD & EXPOSURE",
-    subset: ["Natural", "Human"]
+    subset: ["P2P", "Human"]
+  },
+ // {
+ //   field: "Natural",
+ //   subset: ["P2P"]
+ // },
+  //{
+  //  field: "Natural",
+  //  subset: ["Natural"]
+  // },
+  {
+    field: "P2P",
+    subset: ["Sanitation", "Drinking water", "Hygiene", "WaSH", "Population density", "Urban population growth", "Population living in urban areas", "Population living in slums (% of urban population)", "Household size", "Population"]
   },
   {
-    field: "Natural",
-    subset: ["Earthquake", "Flood", "Tsunami", "Tropical Cyclone", "Drought"]
+    field: "Sanitation",
+    subset: ["Sanitation"]
   },
   {
-    field: "Earthquake",
-    subset: ["Earthquake"]
+    field: "Drinking water",
+    subset: ["Drinking water"]
   },
   {
-    field: "Flood",
-    subset: ["Flood"]
+    field: "Hygiene",
+    subset: ["Hygiene"]
+  },
+    {
+    field: "WaSH",
+    subset: ["WaSH"]
   },
   {
-    field: "Tsunami",
-    subset: ["Tsunami"]
+    field: "Population density",
+    subset: ["Population density"]
+  },
+    {
+    field: "Urban population growth",
+    subset: ["Urban population growth"]
+  },
+    {
+    field: "Population living in urban areas",
+    subset: ["Population living in urban areas"]
+  },
+    {
+    field: "Population living in slums (% of urban population)",
+    subset: ["Population living in slums (% of urban population)"]
+  },
+    {
+    field: "Household size",
+    subset: ["Household size"]
   },
   {
-    field: "Tropical Cyclone",
-    subset: ["Tropical Cyclone"]
-  },
-  {
-    field: "Drought",
-    subset: ["Drought"]
+    field: "Population",
+    subset: ["Population"]
   },
   {
     field: "Human",
